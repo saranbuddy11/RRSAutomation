4b40cb7dccdc707bc6beae082993e86fc36dd895
arjun0110nu <arjun.kumar@nutechnologyinc.com>
2022-08-30 18:09:02 +0530

    updated

--- src/test/java/com/RRS/Runners/TestRunner.java
+++ src/test/java/com/RRS/Runners/TestRunner.java
@@ -5,13 +5,11 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/features/Smoke/",
-        glue = {"com.RRS.StepDefinitions"},
-        dryRun = false,
-        //tags = "@SmokeSuite_Full"
-      //tags = "@login_excel_NImp or @PLP_Full or @PDP_Full or @Login_Full or @Myaccount_RRS or @Search_RRS or @test_feb14"
-        tags = "@RegressionSuite_Full"
-)
-public class TestRunner {}
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features/Smoke/", glue = {
+		"com.RRS.StepDefinitions" }, dryRun = false,
+		// tags = "@SmokeSuite_Full"
+		// tags = "@login_excel_NImp or @PLP_Full or @PDP_Full or @Login_Full or
+		// @Myaccount_RRS or @Search_RRS or @test_feb14"
+		tags = "@RegressionSuite_Full")
+public class TestRunner {
+}
