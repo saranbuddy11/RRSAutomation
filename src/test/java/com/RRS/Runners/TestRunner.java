b473c7b905dafd62292af3c69bbf838bc33b3404
arjun0110nu <arjun.kumar@nutechnologyinc.com>
2023-01-12 16:33:17 +0530

    up

--- src/test/java/com/RRS/Runners/TestRunner.java
+++ src/test/java/com/RRS/Runners/TestRunner.java
@@ -5,11 +5,11 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features/Smoke/", glue = {
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features/Regression/CustomQuery/ValidateSKUImage.feature", glue = {
 		"com.RRS.StepDefinitions" }, dryRun = false,
 		// tags = "@SmokeSuite_Full"
 		// tags = "@login_excel_NImp or @PLP_Full or @PDP_Full or @Login_Full or
 		// @Myaccount_RRS or @Search_RRS or @test_feb14"
-		tags = "@RegressionSuite_Full")
+		tags = "@Regression_RK_001")
 public class TestRunner {
 }
